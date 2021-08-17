ff09b55494c8b2e5f7449c401a15546709ae026a
李成刚 <lichenggang@uniontech.com>
2021-08-17 17:33:16 +0800

    feat: update build

--- build.sh
+++ build.sh
@@ -3,10 +3,11 @@
 sudo docker build -t deepin-shim-review .
 
 id=$(sudo docker create deepin-shim-review)
-sudo docker cp $id:/build/shimx64.efi .
+sudo docker cp $id:/build/shimia32.efi .
+sudo docker cp $id:/build/shimia32.cab .
 sudo docker rm -v $id
 
-sha256sum -c shimx64.efi.sha256sum
+sha256sum -c shimia32.efi.sha256sum
 
 echo "To remove the container image used for this build: sudo docker image rm deepin-shim-review"
 

e879ad27f6782b7dcbe79ad46ddf803891ddded5
zhouzilong <zhouzilong@uniontech.com>
2022-09-05 19:36:54 +0800

    create shim 15.6 review

--- build.sh
+++ build.sh
@@ -9,15 +9,12 @@ if  [ $arch = "x86_64" ];then
 	sudo docker cp $id:/build/shimx64.efi .
 	sudo docker cp $id:/build/shimx64.cab .
 	sudo docker cp $id:/build/shimx64.cksum .
-	sudo docker cp $id:/build/shimia32.efi .
-	sudo docker cp $id:/build/shimia32.cab .
-	sudo docker cp $id:/build/shimia32.cksum .
-	sha256sum -c shimx64.efi.sha256sum
-	sha256sum -c shimia32.efi.sha256sum
+	sha256sum -c shimx64.cksum
 elif [ $arch = "aarch64" ];then
 	sudo docker cp $id:/build/shimaa64.efi .
 	sudo docker cp $id:/build/shimaa64.cab .
 	sudo docker cp $id:/build/shimaa64.cksum .
+	sha256sum -c shimaa64.cksum
 fi
 
 sudo docker rm -v $id
